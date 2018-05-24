95627e1d072e6d84c506550faeb19d72825462a8
parkcitymedia <blakemichaelgaynor@gmail.com>
2018-05-24 11:56:13 -0700

    get hang of functions and variables with pygtk - gtk+3

--- GTK+/gtk3/basic-window/basic-window.py
+++ GTK+/gtk3/basic-window/basic-window.py
@@ -1,13 +1,32 @@
 from gi.repository import Gtk
-gi.require_version('Gtk', '3.0')
 
-class MainWindow(Gtk.Window):
-        def __init__(self):
-            Gtk.Window.__init__(self, title="Button window")
+window = Gtk.Window()
 
-            self.button = Gtk.Button(label="Hey, click me!")
+label = Gtk.Label()
+label.set_label("Label function text")
+label.set_angle(180)
+label.set_halign(Gtk.Align.END)
+window.add(label)
 
-window = MainWindow()
-# window.connect("delete-event", Gtk.main_quit)
-# window.show_all()
-# Gtk.main()
+window.connect("delete-event", Gtk.main_quit)
+window.show_all()
+Gtk.main()
+
+#from gi.repository import Gtk
+#
+#class MainWindow(Gtk.Window):
+#        def __init__(self):
+#            Gtk.Window.__init__(self, title="Button window")
+#            self.button = Gtk.Button(label="Hey, click me!")
+#            self.button.connect("clicked", self.button_clicked)
+#            self.add(self.button)
+#
+#        def button_clicked(self, widget):
+#            print("Hey! did you ask before touching me there?")
+#
+#
+#
+#window = MainWindow()
+#window.connect("delete-event", Gtk.main_quit)
+#window.show_all()
+#Gtk.main()
